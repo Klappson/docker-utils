6a970ddf6815a2c8b9922f1bb226ad7813b217ed
Klappson <freezy111111@gmail.com>
2023-03-15 19:56:56 +0100

    Initial commit

---
+++ klappson/dockerutils/dockerutils.py
@@ -0,0 +1,87 @@
+import os
+import threading
+import subprocess
+from time import sleep
+
+
+class UserConfig:
+    def __init__(self, default, user_copy, symlink):
+        self.default = default
+        self.user_copy = user_copy
+        self.symlink = symlink
+    
+    def remove_symlink(self):
+        if os.path.isfile(self.symlink):
+            subprocess.run(['rm', '-f', self.symlink])
+    
+    def copy_default(self):
+        if not os.path.isfile(self.user_copy):
+            subprocess.run(['cp', self.default, self.user_copy])
+    
+    def create_symlink(self):
+        subprocess.run(['ln', '-s', self.user_copy, self.symlink])
+
+
+class ContainerSetup:
+    def __init__(self):
+        self.default_config_dir = "/root/default_configs"
+
+        self.mount_dirs: list[str] = []
+        self.symlinks: list[UserConfig] = []
+        self.programs: list[list[str]] = []
+        self.program_threads: list[threading.Thread] = []
+    
+    def _run_programms(self, start_delay=3):
+        '''Start calling programms defined in self.programs
+        '''
+        print('\n[CONTAINERSETUP/PROGRAMMS] Running Commands...')
+
+        for program in self.programs:
+            print(
+                f'[CONTAINERSETUP/PROGRAMMS]\tStarting "{" ".join(program)}"')
+            
+            thr = threading.Thread(
+                target=subprocess.run,
+                kwargs={'args': program},
+                name=' '.join(program),
+                daemon=False,
+            )
+
+            self.program_threads.append(thr)
+            sleep(start_delay)
+    
+    def _create_dirs(self):
+        '''Create dirs defined in self.mount_dirs
+        '''
+        print('\n[CONTAINERSETUP/DIRS] Creating Dirs...')
+
+        for mkdir in self.mount_dirs:
+            print(f'[CONTAINERSETUP/DIRS]\tCreating "{mkdir}"')
+            subprocess.run(
+                ['mkdir', '-p', mkdir]
+            )
+    
+    def _create_symlinks(self):
+        '''Create the Symlinks defined in self.symlinks
+        '''
+        print('\n[CONTAINERSETUP/SYMLINKS] Creating Symlink...')
+
+        for symlink in self.symlinks:
+            print((
+                '[CONTAINERSETUP/SYMLINKS]\t'
+                f'{symlink.default} -> '
+                f'{symlink.user_copy} -> '
+                f'{symlink.symlink}'))
+            symlink.remove_symlink()
+            symlink.copy_default()
+            symlink.create_symlink()
+
+    def _print_networkinfo():
+        subprocess.run(['ip', 'a'])
+    
+    def setup(self):
+        ContainerSetup._print_networkinfo()
+
+        self._create_dirs()
+        self._create_symlinks()
+        self._run_programms()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Klappson's Dockerutils",
+    version='0.1',
+    description='Code I wrote for my docker-images',
+    packages=[
+        'klappson',
+        'klappson.dockerutils',
+    ],
+)
